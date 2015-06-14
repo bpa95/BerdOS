2e3bcfbbd3accad40776723bc297d2c6c90cc5fb
bpa95 <berpaval@gmail.com>
2015-06-14 17:45:58 +0300

    simple filter

--- filter/filter.c
+++ filter/filter.c
@@ -1,8 +1,18 @@
 #include "helpers.h"
+#include <stdlib.h>
 
 int main(int argc, char * argv[])
 {
-    char* args[] = {"ls", "/bin", NULL};
-    int res = spawn("ls", args);
-    return res;
+    char** args = (char**) calloc(argc + 1, sizeof(char*));
+    int i;
+    for (i = 0; i < argc - 1; i++) {
+        args[i] = argv[i + 1];
+    }
+    args[argc] = NULL;
+
+    args[argc - 1] = ".";
+    int res = spawn(args[0], args);
+
+    free(args);
+    return 0;
 }
