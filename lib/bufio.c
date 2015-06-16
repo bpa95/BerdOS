81cf55234e4b5c6cbcc1a9eda931cb3bf3b7057e
bpa95 <berpaval@gmail.com>
2015-06-16 14:56:06 +0300

    buf_fill and buf_flush done

--- lib/bufio.c
+++ lib/bufio.c
@@ -1,5 +1,6 @@
 #include "bufio.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct buf_t* buf_new(size_t capacity)
 {
@@ -34,11 +35,44 @@ size_t buf_size(struct buf_t * buf)
 
 ssize_t buf_fill(int fd, struct buf_t * buf, size_t required)
 {
-    return 0;   
+    size_t *size = &buf->size;
+    size_t *capacity = &buf->capacity;
+    void* data = buf->data;
+    while (*size < required) {
+        ssize_t r = read(fd, data + *size, *capacity - *size);
+        if (r == -1) {
+            return -1;
+        } else if (r == 0) {
+            break;
+        } else {
+            *size += r;
+        }
+    }
+    return *size; 
 }
 
 ssize_t buf_flush(int fd, struct buf_t * buf, size_t required)
 {
-    return 0;
+    size_t *size = &buf->size;
+    void* data = buf->data;
+    ssize_t tw = 0;
+    if (required > *size) {
+        required = *size;
+    }
+    while (tw < required) {
+        ssize_t w = write(fd, data + tw, required - tw);
+        if (w == -1) {
+            *size -= tw;
+            memmove(data, data + tw, *size);
+            return -1;
+        } else if (w == 0) {
+            break;
+        } else {
+            tw += w;
+        }
+    }
+    *size -= tw;
+    memmove(data, data + tw, *size);
+    return tw;
 }
 
